86e1a3315facf60c8a64c9573b376ec8a0d74545
Sion Gang <siongang@gmail.com>
2024-07-29 19:48:29 -0400

    ignore

--- midiParser.py
+++ midiParser.py
@@ -1,7 +1,7 @@
 import pretty_midi as pm
 
 #song midi data
-midi_data = pm.PrettyMIDI('Asayake__Casiopea_Mint_Jams_test.mid')
+midi_data = pm.PrettyMIDI('midi/Asayake__Casiopea_Mint_Jams_test.mid')
 
 instruments = midi_data.instruments
 # print(instruments[0].notes)
