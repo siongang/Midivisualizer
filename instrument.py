7a11db9e516c2654f45146b34829547a3ebcf1ed
Sion Gang <siongang@gmail.com>
2024-07-30 23:07:14 -0400

    delete

--- instrument.py
+++
@@ -1,6 +0,0 @@
-class Instrument():
-    def __init__(self, name,  colour, speed, icon):
-        self.name = name
-        self.colour = colour
-        self.speed = speed
-        self.icon = icon
